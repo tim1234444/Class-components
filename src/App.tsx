1a56fae6e8a8c631ed8909b37a43a1ae6020cfa6
tim1234444 <132139591+tim1234444@users.noreply.github.com>
2025-08-21 11:24:33 +0300

    Revert "Class components"

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
 import { Component } from 'react';
-import { RickAndMorty } from './pages/RickAndMorty';
-import { ErrorBoundary } from './components/RickAndMorty/ErrorBoundary';
 
-export default class App extends Component {
-  
+export default class Counter extends Component<{
+  name:string,
+  age:string
+}> {
+  constructor( props :{
+    name:string,
+    age:string
+  }){
+    super( props );
+  }
+  state = {
+    name: this.props.name,
+    age: +this.props.age,
+  };
+
+  handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+  handleAgeChange = () => {
+    this.setState({
+      age: this.state.age + 1 
+    });
+  };
 
   render() {
     return (
       <>
-      <ErrorBoundary>
-      <RickAndMorty />
-    </ErrorBoundary>
+        <input
+          value={this.state.name}
+          onChange={this.handleNameChange}
+        />
+        <button onClick={this.handleAgeChange}>
+          Increment age
+        </button>
+        <p>Hello, {this.state.name}. You are {this.state.age}.</p>
       </>
     );
   }
